7ad04d1fc7b8efdc3cf2094afcc196c1312d012c
bbsmith24 <bbsmith024@gmail.com>
2019-08-15 08:42:36 -0400

    throughput improvements
    configure for minimal GPS messaging at start, always read but only log when started - minimizes giant wad of GPS data on start when it hasn't been read for a while

--- Yamura_Logger/Yamura_Logger.ino
+++ Yamura_Logger/Yamura_Logger.ino
@@ -121,6 +121,23 @@ void setup()
     Serial.println("GPS OK");
     digitalWrite(GPSPIN, HIGH);
   }
+  /*
+  Supported NMEA Sentences
+  0 NMEA_SEN_GLL, // GPGLL interval - Geographic Position - Latitude longitude    - SKIPPED by viewer
+  1 NMEA_SEN_RMC, // GPRMC interval - Recommended Minimum Specific GNSS Sentence  - PARSED by viewer
+  2 NMEA_SEN_VTG, // GPVTG interval - Course over Ground and Ground Speed         - PARSED by viewer
+  3 NMEA_SEN_GGA, // GPGGA interval - GPS Fix Data                                - PARSED by viewer (but redundant mostly except for satellite count)
+  4 NMEA_SEN_GSA, // GPGSA interval - GNSS DOPS and Active Satellites             - SKIPPED by viewer
+  5 NMEA_SEN_GSV, // GPGSV interval - GNSS Satellites in View                     - SKIPPED by viewer
+  6-16 //Reserved
+  17 NMEA_SEN_ZDA, // GPZDA interval – Time & Date                                - SKIPPED by viewer
+  18 NMEA_SEN_MCHN, // PMTKCHN interval – GPS channel status                      - SKIPPED by viewer
+  */
+  // set to return only RMC and VTG sentences - bare min info
+  //String configString;
+  //configString = myI2CGPS.createMTKpacket(314, ",0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+  //myI2CGPS.sendMTKpacket(configString);
+  myI2CGPS.sendMTKpacket(myI2CGPS.createMTKpacket(314, ",0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"));
 
   Serial.println("Ready!");
   digitalWrite(READYPIN, HIGH);
@@ -129,69 +146,104 @@ void setup()
 void loop() 
 {
   CheckStart();
+  LogData();
+}
+void LogPrint(char c)
+{
   if(logging == true)
   {
-    LogData();
+    dataFile.print(c);
+  }
+}
+void LogPrint(String s)
+{
+  if(logging == true)
+  {
+    dataFile.print(s);
+  }
+}
+void LogWrite(char* b, int len)
+{
+  if(logging == true)
+  {
+    dataFile.write(b, len);
   }
 }
 void LogData()
 {
-  dataFile.print("T");
+  //dataFile.print("T");
+  LogPrint("T");
   i2cData.ul[0] = micros();
-  dataFile.write(i2cData.c[0], 4);
+  //dataFile.write(i2cData.c[0], 4);
+  LogWrite(i2cData.c[0], 4);
 
   // request  bytes from slave device
   // slave may send less than requested
   int a2dcnt = Wire.requestFrom(ANALOG1, A2D_MSG_BLOCK * 4);
+  int byteIdx = 0;
   if(a2dcnt == (A2D_MSG_BLOCK * 4))
   {
+    DataPacket channelNum;
     for(int msgCnt = 0; msgCnt < A2D_MSG_BLOCK; msgCnt++) 
     {
-      Serial.print("A2D");
-      Serial.print(msgCnt);
-      dataFile.print("A2D");
-      i2cData.ul[0] = msgCnt;
-      dataFile.write(i2cData.c[0], 4);
       for(int byteCnt = 0; byteCnt < 4; byteCnt++) 
       {
         // receive a byte as character
         i2cData.c[0][byteCnt] = Wire.read(); 
       }
+      LogPrint("A2D");
+      channelNum.ul[0] = msgCnt;
+      LogWrite(channelNum.c[0], 4);
+      LogWrite(i2cData.c[0], 4);
+      /*
+       * 
+      Serial.print("A2D");
+      Serial.print(msgCnt);
+      Serial.print(" ");
       Serial.print(i2cData.ul[0]);
-      dataFile.write(i2cData.c[0], 4);
+      Serial.print(" ");
+      Serial.print(i2cData.c[0]);
+      Serial.print(" ");
+      Serial.print(i2cData.c[1]);
+      Serial.print(" ");
+      Serial.print(i2cData.c[2]);
+      Serial.print(" ");
+      Serial.print(i2cData.c[3]);
+      Serial.print(" ");
+      */
     }
-    Serial.println("");
+    //Serial.println("");
   }
-  else if (a2dcnt != (A2D_MSG_BLOCK * 4))
-  {
-    Serial.println("received " + String(a2dcnt) + " bytes");  
-  }
+  //else if (a2dcnt != (A2D_MSG_BLOCK * 4))
+  //{
+  //  Serial.println("received " + String(a2dcnt) + " bytes");  
+  //}
   //Serial.print(" ACC ");
   if (accel.available()) 
   {
-    dataFile.print("ACC");
+    LogPrint("ACC");//dataFile.print("ACC");
     i2cData.ul[0] = 1;
-    dataFile.write(i2cData.c[0], 4);
+    LogWrite(i2cData.c[0], 4);//dataFile.write(i2cData.c[0], 4);
     i2cData.f[0] = accel.getCalculatedX();
-    dataFile.write(i2cData.c[0], 4);
+    LogWrite(i2cData.c[0], 4);//dataFile.write(i2cData.c[0], 4);
     i2cData.f[0] = accel.getCalculatedY();
-    dataFile.write(i2cData.c[0], 4);
+    LogWrite(i2cData.c[0], 4);//dataFile.write(i2cData.c[0], 4);
     i2cData.f[0] = accel.getCalculatedZ();
-    dataFile.write(i2cData.c[0], 4);
+    LogWrite(i2cData.c[0], 4);//dataFile.write(i2cData.c[0], 4);
   }
 
   //Serial.print(" GPS ");
   if(myI2CGPS.available()>0) //available() returns the number of new bytes available from the GPS module
   {
     char c;
-    dataFile.print("GPS");
+    LogPrint("GPS");//dataFile.print("GPS");
     i2cData.ul[0] = 2;
-    dataFile.write(i2cData.c[0], 4);
+    LogWrite(i2cData.c[0], 4);//dataFile.write(i2cData.c[0], 4);
     // read NMEA sentence and write to log - easier to deal with on the viewer side
     while (myI2CGPS.available()) //available() returns the number of new bytes available from the GPS module
     {
       c = myI2CGPS.read();
-      dataFile.print(c);
+      LogPrint(c);//dataFile.print(c);
     }
   }
   dataFile.flush();
